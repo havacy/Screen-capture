f2e2b8264dca3ada3d94b575241df36f47f6c4b8
havacy <96433789+havacy@users.noreply.github.com>
2024-10-29 23:12:32 -0400

    Add files via upload

---
+++ E.py
@@ -0,0 +1,25 @@
+import cv2
+import numpy as np
+import pyscreeze
+from pynput.mouse import Controller, Button
+import time
+import threading
+
+x1, y1 = 836, 444
+x2, y2 = 1050, 640
+
+def capture_region(x1, y1, x2, y2):
+    screenshot = pyscreeze.screenshot(region=(x1, y1, x2 - x1, y2 - y1))
+    frame = np.array(screenshot)
+    return frame
+
+while True:
+    frame = capture_region(x1, y1, x2, y2)
+
+
+    cv2.imshow("Capture Region", frame)
+
+    if cv2.waitKey(1) & 0xFF == ord('q'):
+        break
+
+cv2.destroyAllWindows()
